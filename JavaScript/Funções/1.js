726c1b7d3e88b56f73847a5d8d1c734f747764ff
Laura Mafra <imlauramafra@gmail.com>
2026-06-15 08:17:05 -0300

    Delete JavaScript directory

--- JavaScript/Funções/1.js
+++
@@ -1,5 +0,0 @@
-const dobro = (numero) => numero * 2;
-
-// Testando a função:
-console.log(dobro(5));  // Saída: 10
-console.log(dobro(12)); // Saída: 24
--- JavaScript/Funções/4.js
+++
@@ -1,5 +0,0 @@
-const converterTemperatura = (celsius) => (celsius * 9 / 5) + 32;
-
-// Testando a função:
-console.log(converterTemperatura(30)); // Saída: 86
-console.log(converterTemperatura(0));  // Saída: 32
